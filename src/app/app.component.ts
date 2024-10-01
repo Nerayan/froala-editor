cfecd27a9ce3994b5adc2d2f1345897fed069dcf
Roman Holovchuk <r.golovchuk@gmail.com>
2024-10-01 22:53:39 +0300

    froala config for Cyber

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,115 @@
 import { Component } from '@angular/core';
+import * as Prism from 'prismjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'froala';
   editorContent: string = '';
   froalaEditorOptions: Object = {
-    pluginsEnabled: ['align', 'charCounter', 'codeBeautifier', 'codeView', 'colors', 'draggable', 'emoticons', 'entities', 'fontSize', 'fullscreen', 'image', 'imageManager', 'inlineStyle', 'lineBreaker', 'link', 'lists', 'paragraphFormat', 'paragraphStyle', 'quote', 'save', 'table', 'url', 'specialCharacters', 'video', 'embedly'],
-    toolbarButtons: ['undo', 'redo', 'paragraphFormat', 'paragraphStyle', 'backgroundColor', 'textColor', 'fontSize', 'bold', 'underline', 'strikeThrough', 'subscript', 'superscript', 'italic', 'specialCharacters', 'insertLink', 'align', 'formatULSimple', 'formatOLSimple', 'quote', 'insertImage', 'insertTable', 'insertVideo']
+    pluginsEnabled: [
+      'align',
+      'charCounter',
+      'codeBeautifier',
+      'codeView',
+      'colors',
+      'draggable',
+      'emoticons',
+      'entities',
+      'fontSize',
+      'fullscreen',
+      'image',
+      'imageManager',
+      'inlineStyle',
+      'lineBreaker',
+      'link',
+      'lists',
+      'listsAdvanced',
+      'paragraphFormat',
+      'paragraphStyle',
+      'quote',
+      'save',
+      'table',
+      'url',
+      'specialCharacters',
+      'video',
+      'embedly',
+      'tableResize',
+    ],
+    toolbarButtons: [
+      'undo',
+      'redo',
+      'paragraphFormat',
+      'paragraphStyle',
+      'backgroundColor',
+      'textColor',
+      'fontSize',
+      'bold',
+      'underline',
+      'strikeThrough',
+      'subscript',
+      'superscript',
+      'italic',
+      'clearFormatting',
+      'specialCharacters',
+      'insertLink',
+      'align',
+      'formatOLSimple',
+      'formatUL',
+      'quote',
+      'selectAll',
+      'html',
+      'insertImage',
+      'insertTable',
+      'insertVideo',
+      'codeBlock',
+    ],
+    codeBeautifierOptions: {
+      end_with_newline: true,
+      indent_size: 2,
+      indent_char: ' ',
+      preserve_newlines: true,
+      max_preserve_newlines: 10,
+    },
+    codeLanguages: [
+      { code: 'html', name: 'HTML' },
+      { code: 'javascript', name: 'JavaScript' },
+      { code: 'typescript', name: 'TypeScript' },
+      { code: 'css', name: 'CSS' },
+      { code: 'java', name: 'Java' },
+      // додай інші мови
+    ],
+    colorsBackground:[
+      '#FFFFFF','#000000','#86BC25','#26890D','#007CB0','#0df200','#33f0ff',
+      '#DA291C','#ED8B00','#FFCD00','#E3E48D','#C4D600','#009A44','#2C5234',
+      '#A0DCFF','#62B5E5','#00A3E0','#0076A8','#005587','#012169','#041E42',
+      '#D0D0CE','#BBBCBC','#A7A8AA','#97999B','#75787B','#63666A','#53565A',
+      'REMOVE'
+    ],
+    colorsText:[
+      '#FFFFFF','#000000','#86BC25','#26890D','#007CB0','#0df200','#33f0ff',
+      '#DA291C','#ED8B00','#FFCD00','#E3E48D','#C4D600','#009A44','#2C5234',
+      '#A0DCFF','#62B5E5','#00A3E0','#0076A8','#005587','#012169','#041E42',
+      '#D0D0CE','#BBBCBC','#A7A8AA','#97999B','#75787B','#63666A','#53565A',
+      'REMOVE'
+    ],
+    typingTimer: 1000,
+    listOptions: {
+      level1: '•',
+      level2: '-',
+    },
+    tableResize: true,
+    events: {
+      'codeView.update': function () {
+        Prism.highlightAll();
+      },
+      'table.inserted': function (table: any) {
+        const caption = table.createCaption();
+        caption.innerText = 'Enter table caption';
+      },
+    },
   };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
-import "froala-editor/js/plugins/align.min.js";
-import "froala-editor/js/plugins/char_counter.min.js";
-import "froala-editor/js/plugins/colors.min.js";
-import "froala-editor/js/plugins/special_characters.min.js";
-import "froala-editor/js/plugins/font_size.min.js";
-import "froala-editor/js/plugins/image.min.js";
-import "froala-editor/js/plugins/image_manager.min.js";
-import "froala-editor/js/plugins/line_breaker.min.js";
-import "froala-editor/js/plugins/link.min.js";
-import "froala-editor/js/plugins/lists.min.js";
-import "froala-editor/js/plugins/paragraph_style.min.js";
-import "froala-editor/js/plugins/paragraph_format.min.js"
-import "froala-editor/js/plugins/quick_insert.min.js";
-import "froala-editor/js/plugins/quote.min.js";
-import "froala-editor/js/plugins/table.min.js";
-import "froala-editor/js/plugins/url.min.js";
-import "froala-editor/js/plugins/video.min.js";
+import 'froala-editor/js/plugins/align.min.js';
+import 'froala-editor/js/plugins/char_counter.min.js';
+import 'froala-editor/js/plugins/code_beautifier.min.js';
+import 'froala-editor/js/plugins/code_view.min.js';
+import 'froala-editor/js/plugins/colors.min.js';
+import 'froala-editor/js/plugins/draggable.min.js';
+import 'froala-editor/js/plugins/emoticons.min.js';
+import 'froala-editor/js/plugins/entities.min.js';
+import 'froala-editor/js/plugins/font_size.min.js';
+import 'froala-editor/js/plugins/fullscreen.min.js';
+import 'froala-editor/js/plugins/image.min.js';
+import 'froala-editor/js/plugins/image_manager.min.js';
+import 'froala-editor/js/plugins/inline_style.min.js';
+import 'froala-editor/js/plugins/line_breaker.min.js';
+import 'froala-editor/js/plugins/link.min.js';
+import 'froala-editor/js/plugins/lists.min.js';
+import 'froala-editor/js/plugins/paragraph_format.min.js';
+import 'froala-editor/js/plugins/paragraph_style.min.js';
+import 'froala-editor/js/plugins/quote.min.js';
+import 'froala-editor/js/plugins/save.min.js';
+import 'froala-editor/js/plugins/table.min.js';
+import 'froala-editor/js/plugins/url.min.js';
+import 'froala-editor/js/plugins/special_characters.min.js';
+import 'froala-editor/js/plugins/video.min.js';
+import 'froala-editor/js/plugins/table.min.js';
 
 @NgModule({
   declarations: [
